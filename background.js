9fd6fd697bea4274da0f98915c7dd064b9ebe0b8
Gavin C <59036563+Gavin-TC@users.noreply.github.com>
2023-03-04 21:42:32 -0500

    working, needs to replace text

--- background.js
+++ background.js
@@ -1,1 +1,96 @@
 console.log("background script is running fine");
+// a background script monitors events i the browser (i.e. selecting text)
+
+
+chrome.contextMenus.create({
+    title: "Convert to Cyrillic",
+    id: "convert_cyrillic",
+    contexts: ['selection'],
+});
+
+chrome.contextMenus.create({
+    title: "Convert to Latin phonics",
+    id: "convert_latin",
+    contexts: ['selection'],
+});
+
+
+chrome.contextMenus.onClicked.addListener(function(info, tab) {
+    if (info.menuItemId === 'convert_cyrillic') {
+        convertCyrillic(tab, info.selectionText);
+    }
+    if (info.menuItemId === 'convert_latin') {
+        convertLatin(tab, info.selectionText);
+    }
+});
+
+
+var cyrillic = [
+    "я", "ц", "у", "к", "е", "н", "г", "ш", "щ",
+    "ж", "х", "ъ", "ф", "ы", "в", "а", "п", "р", "о",
+    "л", "д", "з", "э", "й", "ч", "с", "м", "и", "т",
+    "ь", "б", "ю", "к"
+];
+var cyrillic_upper = [
+    "Я", "Ц", "У", "К", "Е", "Н", "Г", "Ш", "Щ",
+    "Ж", "Х", "Ъ", "Ф", "Ы", "В", "А", "П", "Р", "О",
+    "Л", "Д", "З", "Э", "Й", "Ч", "С", "М", "И", "Т",
+    "Ь", "Б", "Ю", "К"
+];
+
+
+var phonics = [
+    "ya", "ts", "u", "c", "ye", "n", "g", "sh", "sha",
+    "zh", "kh", '"', "f", "y'", "v", "a", "p", "r", "o",
+    "l", "d", "z", "e", "y", "ch", "s", "m", "i", "t",
+    "'", "b", "yu", "k"
+];
+var phonics_upper = [
+    "Ya", "Ts", "U", "C", "Ye", "N", "G", "Sh", "Sha",
+    "Zh", "Kh", '"', "F", "Y'", "V", "A", "P", "R", "O",
+    "L", "D", "Z", "E", "Y", "Ch", "S", "M", "I", "T",
+    "'", "B", "Yu", "K"
+];
+
+
+function convertCyrillic(tab, selected_text) {
+    original_text = selected_text;
+
+    for (let i = 0; i < selected_text.length; i++) { // check every character inside of the selected_text
+        for (let j = 0; j < phonics.length; j++) { // check if the current character contains an item from the phonics array, capitals first
+            if(selected_text.includes(phonics_upper[j])) {
+                selected_text = selected_text.replace(phonics_upper[j], cyrillic_upper[j]);
+            }
+
+            if (selected_text.includes(phonics[j])) { // replace the character that matches the corresponding phonic with the corresponding cyrillic character
+                selected_text = selected_text.replace(phonics[j], cyrillic[j]);
+            }
+        }
+    }
+    converted_text = selected_text;
+
+    console.log(selected_text + ": convert cyrillic");
+    chrome.tabs.sendMessage(tab.id, { type: 'replaceSelection', text: converted_text });
+}
+
+
+function convertLatin(tab, selected_text) {
+    original_text = selected_text; 
+
+    for (let i = 0; i < selected_text.length; i++) { // check every character inside of the selected_text
+        for (let j = 0; j < cyrillic.length; j++) { // check if the current character contains an item from the cyrillic array
+            if(selected_text.includes(cyrillic_upper[j])) {
+                selected_text = selected_text.replace(cyrillic_upper[j], phonics_upper[j]);
+            }
+
+            if (selected_text.includes(cyrillic[j])) { // replace the character that matches the corresponding phonic with the corresponding cyrillic character
+                selected_text = selected_text.replace(cyrillic[j], phonics[j]);
+            }
+        }
+    }
+
+    converted_text = selected_text;
+
+    console.log(selected_text + ": convert latin");
+    chrome.tabs.sendMessage(tab.id, { type: 'replaceSelection', text: converted_text });
+}
