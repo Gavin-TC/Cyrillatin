ec37140dc0e54c7c769fa414bbe1b92ef3c40621
Gavin C <59036563+Gavin-TC@users.noreply.github.com>
2023-03-04 21:42:37 -0500

    upload

--- content.js
+++ content.js
@@ -1,1 +1,30 @@
-console.log("content script is running fine");
+converted_text = "";
+
+
+chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
+    if (message.type === 'replaceSelection') {
+
+        var selection = document.getSelection().toString();
+        var all_elements = document.getRootNode();
+
+        selection.replace(selection, converted_text);
+
+        //selection.replace("gavin", converted_text);
+        console.log("SELECTION: " + selection);
+        
+    }
+});
+
+
+// chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+//     if (message != null) {
+//         converted_text = message;
+//         replace_text("hello", converted_text);
+//         sendResponse({message : "Response from content.js"})
+//     }
+// });
+
+
+function replace_text(replacement_text, new_text) {
+    // replacement_text = new_text
+}
