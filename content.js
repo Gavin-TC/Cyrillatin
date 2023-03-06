ca9f59285f13e1d69f3bc42ed732fd42e080f89c
Gavin C <59036563+Gavin-TC@users.noreply.github.com>
2023-03-06 16:07:19 -0500

    still can't replace text : (

--- content.js
+++ content.js
@@ -1,18 +1,26 @@
+console.log("content.js is running");
 converted_text = "";
 
 
 chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
+    converted_text = message;
+
     if (message.type === 'replaceSelection') {
-
         var selection = document.getSelection().toString();
-        var all_elements = document.getRootNode();
-
-        selection.replace(selection, converted_text);
-
-        //selection.replace("gavin", converted_text);
-        console.log("SELECTION: " + selection);
-        
+        console.log(selection);
     }
+    // if (message.type === 'replaceSelection') {
+    //     converted_text = message;
+    //     const text = document.querySelectorAll('h1, h2, h3, h4, h5, p, li, td, caption, span, a');
+    //     var selection = document.getSelection().toString();
+    //     console.log("text : " + text);
+    //     console.log("selection : " + selection);
+    //     console.log("converted text : " + converted_text);
+    //     // var sel_range = selection.getRangeAt(0);
+    //     // sel_range.
+    //     // //selection.replace("gavin", converted_text);
+    //     // console.log("SELECTION: " + selection);
+    // }
 });
 
 
