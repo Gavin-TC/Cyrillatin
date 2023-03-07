336313806cdc2caa3602520f2f745b173c1c8ad9
Gavin C <59036563+Gavin-TC@users.noreply.github.com>
2023-03-07 15:19:28 -0500

    testing

--- content.js
+++ content.js
@@ -3,36 +3,54 @@ converted_text = "";
 
 
 chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
-    converted_text = message;
+    msg = message.text.toString();
+    converted_text = msg;
+    console.log("CONVERTED TEXT : " + converted_text);
+    console.log("MSG TYPE : " + typeof msg);
 
     if (message.type === 'replaceSelection') {
         var selection = document.getSelection().toString();
-        console.log(selection);
+        replace_text(selection, converted_text);
+        
     }
-    // if (message.type === 'replaceSelection') {
-    //     converted_text = message;
-    //     const text = document.querySelectorAll('h1, h2, h3, h4, h5, p, li, td, caption, span, a');
-    //     var selection = document.getSelection().toString();
-    //     console.log("text : " + text);
-    //     console.log("selection : " + selection);
-    //     console.log("converted text : " + converted_text);
-    //     // var sel_range = selection.getRangeAt(0);
-    //     // sel_range.
-    //     // //selection.replace("gavin", converted_text);
-    //     // console.log("SELECTION: " + selection);
-    // }
 });
 
 
-// chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-//     if (message != null) {
-//         converted_text = message;
-//         replace_text("hello", converted_text);
-//         sendResponse({message : "Response from content.js"})
-//     }
-// });
+function convert(elem) {
+    var content = document.body.innerHTML; // get HTML content for the given element
+    var pattern = new RegExp(/hello/gi);
+    content = content.replace(pattern,'hi');
+    document.getElementById(elem).innerHTML = content; // put the replace content back
+}
 
 
-function replace_text(replacement_text, new_text) {
-    // replacement_text = new_text
+function replaceTextOnPage(from, to){
+    getAllTextNodes().forEach(function(node){
+      node.nodeValue = node.nodeValue.replace(new RegExp(quote(from), 'g'), to);
+    });
+  
+    function getAllTextNodes(){
+      var result = [];
+  
+      (function scanSubTree(node){
+        if(node.childNodes.length) 
+          for(var i = 0; i < node.childNodes.length; i++) 
+            scanSubTree(node.childNodes[i]);
+        else if(node.nodeType == Node.TEXT_NODE) 
+          result.push(node);
+      })(document);
+  
+      return result;
+    }
+  
+    function quote(str){
+      return (str+'').replace(/([.?*+^$[\]\\(){}|-])/g, "\\$1");
+    }
+}
+
+
+function replace_text(text_to_replace, replacement_text) {
+    document.body.innerText.replace(text_to_replace, replacement_text);
+
+    console.log("ATTEMPTED REPLACEMENT OF TEXT.");
 }
