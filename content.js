ecd6bee5fdf3e3af336562e1c10e75087bfde1f8
Gavin C <59036563+Gavin-TC@users.noreply.github.com>
2023-03-09 13:56:24 -0500

    replacing

--- content.js
+++ content.js
@@ -12,25 +12,6 @@ chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
 
 
 function replace_text(text_to_replace, replacement_text) {
-<<<<<<< Updated upstream
     const textarea = document.querySelector('textarea');
     textarea.value = textarea.value.replace(text_to_replace, replacement_text);
-=======
-    const text = document.querySelectorAll('h1, h2, h3, h4, h5, p, li, td, caption, span, a');
-    console.log(document.querySelectorAll('h1, h2, h3, h4, h5, p, li, td, caption, span, a').forEach);
-
-    for (let i = 0; i < text.length; i++) {
-
-      console.log(text[i].innerHTML.includes('ponyal'));
-
-      if (text[i].innerHTML.includes('ponyal')) {
-        text[i].innerHTML = text[i].innerHTML.replace(text_to_replace, replacement_text);
-
-      } else {
-        console.log("this returned false");
-        return false;
-
-      }
-    }
->>>>>>> Stashed changes
 }
