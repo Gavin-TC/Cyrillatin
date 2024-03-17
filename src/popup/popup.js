9e8b4d73b6bd78064f0eac68399d8a30aff05e6b
Gavin TC <gtclonts@gmail.com>
2024-03-17 18:33:22 -0400

    fixed bug with github button

--- src/popup/popup.js
+++ src/popup/popup.js
@@ -1,10 +1,4 @@
 document.addEventListener("DOMContentLoaded", function() {
-    document.querySelector('.donate-button').addEventListener('click', function() {
-        chrome.tabs.create({
-            url: "https://www.buymeacoffee.com/gavintclonts"
-        });
-    });
-    
     document.querySelector('.github-button').addEventListener('click', function() {
         chrome.tabs.create({
             url: "https://github.com/Gavin-TC/Cyrillatin"
